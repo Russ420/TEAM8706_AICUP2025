05c74288bb86c21da88e1585c7ec439ea0ea9513
Russ420 <captain31526@gmail.com>
2025-09-29 06:10:38 +0800

    Add Feature
    Objective:
    	Create a directory for storing the script
    
    Details:
    	Create a env.sh to activate the environment variable for the nnUNet

---
+++ script/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export nnUNet_raw="/work/u4526725/Dataset/nnUNet/nnUNet_raw"
+export nnUNet_preprocessed="/work/u4526725/Dataset/nnUNet/nnUNet_preprocessed"
+export nnUNet_results="/work/u4526725/Dataset/nnUNet/nnUNet_results"
+echo $nnUNet_raw
+echo $nnUNet_preprocessed
+echo $nnUNet_results
