22264322135deb705814152fa2edf4475dc9623c
Russ420 <captain31526@gmail.com>
2025-11-10 18:57:53 +0800

    Add Feature
    Add the path of nnUNet and nnUNet_predictions to prevent typing lots of cd

--- script/nchc/env.sh
+++ script/nchc/env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+export nnUNet="/work/u4526725/ntuh/nnUNet"
 export nnUNet_raw="/work/u4526725/Dataset/nnUNet/nnUNet_raw"
 export nnUNet_preprocessed="/work/u4526725/Dataset/nnUNet/nnUNet_preprocessed"
 export nnUNet_results="/work/u4526725/Dataset/nnUNet/nnUNet_results"
+export nnUNet_predictions="/work/u4526725/Dataset/nnUNet/nnUNet_predictions"
 
 PATH="$PATH:/home/u4526725/.local/bin"
+echo $nnUNet
 echo $nnUNet_raw
 echo $nnUNet_preprocessed
 echo $nnUNet_results
+echo $nnUNet_predictions
 echo $PATH
