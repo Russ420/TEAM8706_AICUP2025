36ab9ca9e5d6218e7f72ad08ab12cb4fc3e9d700
Russ420 <captain31526@gmail.com>
2025-11-10 19:02:47 +0800

    Modify Feature
    Modify train script. Following log files for different configurations.

--- script/nchc/twnia2/train.sh
+++ script/nchc/twnia2/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --account=MST114281                             # parent project to access twcc system
-#SBATCH --job-name=train_3dfullres                        # jobName
-#SBATCH --nodes=1                                       # request node number
-#SBATCH --ntasks-per-node=1                             # number of tasks can execute on the node
-#SBATCH --gpus-per-node=1                               # gpus per node
-#SBATCH --cpus-per-task=4                               # cpus per gpu
-#SBATCH --partition=gp1d                               # how long task can run
-#SBATCH --output=./log/twnia2/train_3dfullres.out   # specify output Diectory and fileName
+#SBATCH --account=MST114281                                     # parent project to access twcc system
+#SBATCH --job-name=train_3d_cascade_fullres                     # jobName
+#SBATCH --nodes=1                                               # request node number
+#SBATCH --ntasks-per-node=1                                     # number of tasks can execute on the node
+#SBATCH --gpus-per-node=1                                       # gpus per node
+#SBATCH --cpus-per-task=4                                       # cpus per gpu
+#SBATCH --partition=gp2d                                        # how long task can run
+#SBATCH --output=./log/twnia2/train_3d_cascade_fullres.out      # specify output Diectory and fileName
 
 # environment setting
 module purge
@@ -16,5 +16,17 @@ conda activate nnUNet
 ### This shell script is edited by Russ as template script for other job ###
 echo $(date)
 source script/nchc/env.sh
+
 ### fold might not contain any class 3 (cal)
-nnUNetv2_train 306 3d_fullres 0 
+N_FOLD="all"
+### ensure the suffix of the job name and log file are identical
+CONFIG="3d_cascade_fullres"
+
+# DICE + CE
+#nnUNetv2_train 306 2d $N_FOLD 
+#nnUNetv2_train 306 3d_fullres $N_FOLD 
+#nnUNetv2_train 306 3d_lowres $N_FOLD 
+nnUNetv2_train 306 $CONFIG $N_FOLD 
+
+# DICE + Focal
+#nnUNetv2_train 306 3d_fullres $N_FOLD -tr
