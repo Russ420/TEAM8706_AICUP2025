31e1c11ec6bd4faeb814989f0045cffc2341d876
Russ420 <captain31526@gmail.com>
2025-12-01 00:23:20 +0800

    Add feature
    We add up the predict.sh for original nnUNet and the predict_fc.sh for
    our focal loss variant. So as train/train_fc.sh

--- script/nchc/twnia2/predict_fc.sh
+++ script/nchc/twnia2/predict_fc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account=MST114281                             	# parent project to access twcc system
-#SBATCH --job-name=predict_3d_fullres_best           		# jobName
+#SBATCH --job-name=predict_fc_3d_fullres                    # jobName
 #SBATCH --nodes=1                                       	# request node number
 #SBATCH --ntasks-per-node=1                             	# number of tasks can execute on the node
 #SBATCH --gpus-per-node=1                               	# gpus per node
 #SBATCH --cpus-per-task=4                               	# cpus per gpu
 #SBATCH --partition=gp1d                              		# how long task can run
-#SBATCH --output=./log/twnia2/predict_3d_fullres_best.out 	# specify output Diectory and fileName
+#SBATCH --output=./log/twnia2/predict_fc_3d_fullres.out 	# specify output Diectory and fileName
 
 # environment setting
 module purge
@@ -21,8 +21,10 @@ source script/nchc/env.sh
 DIR_INPUT="$nnUNet_raw/Dataset306_aicup2025/imagesTs"
 
 ## vanilla
-DIR_OUTPUT="$nnUNet_predictions/Dataset306_aicup2025/nnUNetTrainer__nnUNetPlans__$CONFIG"
-nnUNetv2_predict -i $DIR_INPUT -o $DIR_OUTPUT -d 306 -c $CONFIG -chk checkpoint_best.pth
+#DIR_OUTPUT="$nnUNet_predictions/Dataset306_aicup2025/nnUNetTrainer__nnUNetPlans__$CONFIG"
+#nnUNetv2_predict -i $DIR_INPUT -o $DIR_OUTPUT -d 306 -c $CONFIG -chk checkpoint_best.pth
+DIR_OUTPUT="$nnUNet_predictions/Dataset306_aicup2025/nnUNetTrainerFocalLoss__nnUNetPlans__$CONFIG"
+nnUNetv2_predict -i $DIR_INPUT -o $DIR_OUTPUT -d 306 -c $CONFIG -tr nnUNetTrainerFocalLoss -chk checkpoint_best.pth
 
 # vanilla cascade
 #DIR_PREV="$nnUNet_predictions/Dataset306_aicup2025/nnUNetTrainer__nnUNetPlans__3d_lowres"
--- script/nchc/twnia2/train_fc.sh
+++ script/nchc/twnia2/train_fc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account=MST114281                                     # parent project to access twcc system
-#SBATCH --job-name=train_res3d_fullres_fold_0                   # jobName
+#SBATCH --job-name=train_fc_3d_fullres_fold_0_3focal_1dice      # jobName
 #SBATCH --nodes=1                                               # request node number
 #SBATCH --ntasks-per-node=1                                     # number of tasks can execute on the node
 #SBATCH --gpus-per-node=1                                       # gpus per node
 #SBATCH --cpus-per-task=4                                       # cpus per gpu
-#SBATCH --partition=gp4d                                        # how long task can run
-#SBATCH --output=./log/twnia2/train_res3d_fullres_fold_0.out    # specify output Diectory and fileName
+#SBATCH --partition=gp2d                                        # how long task can run
+#SBATCH --output=./log/twnia2/train_fc_3d_fullres_fold_0.out    # specify output Diectory and fileName
 
 # environment setting
 module purge
@@ -23,7 +23,7 @@ N_FOLD=0
 CONFIG="3d_fullres"
 
 # vinalla
-#nnUNetv2_train 306 $CONFIG $N_FOLD 
+nnUNetv2_train 306 $CONFIG $N_FOLD -tr nnUNetTrainerFocalLoss
 
 # residual encoders
-nnUNetv2_train 306 $CONFIG $N_FOLD -p nnUNetResEncUNetLPlans
+#nnUNetv2_train 306 $CONFIG $N_FOLD -p nnUNetResEncUNetLPlans -tr nnUNetTrainerFocalLoss
